bd3744604ad85991361f9f58fc064b3e0c8fac09
Emanuele Blanco <emanuele.blanco@gmail.com>
2013-09-20 23:29:46 +0100

    Handled winning hands - needs refactoring

--- src/main/scala/pokerhands/PokerHands.scala
+++ src/main/scala/pokerhands/PokerHands.scala
@@ -2,10 +2,50 @@ package pokerhands
 
 object PokerHands {
 
+  case class Winner(name: String, hand: String)
+
+  val cards = Map(
+    "2" -> "Two",
+    "3" -> "Three",
+    "4" -> "Four",
+    "5" -> "Five",
+    "6" -> "Six",
+    "7" -> "Seven",
+    "8" -> "Eight",
+    "9" -> "Nine",
+    "J" -> "Jack",
+    "Q" -> "Queen",
+    "K" -> "King",
+    "A" -> "Ace"
+  )
+
   def winner(hands: String): String = {
 
+    object CardOrdering extends Ordering[String] {
+      val order = List('2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A')
+      def compare(x: String, y: String): Int = {
+        if (x == y) 0
+        else order.indexOf(x(0)) - order.indexOf(y(0))
+      }
+    }
+
     def winningPlayer(blackCards: List[String], whiteCards: List[String]) = {
-      if (containsAce(blackCards)) "Black" else "White"
+      if (containsAce(blackCards)) Winner("Black", "with high card: Ace")
+      else if (containsAce(whiteCards)) Winner("White", "with high card: Ace")
+      else {
+        val winner = highCard(blackCards, whiteCards)
+        Winner(winner._1, s"with high card: ${cards(winner._2.substring(0, 1))}")
+      }
+    }
+
+    def highCard(blackCards: List[String], whiteCards: List[String]): (String, String) = {
+      val black = ("Black", blackCards.max(CardOrdering))
+      val white = ("White", whiteCards.max(CardOrdering))
+      CardOrdering.compare(black._2, white._2) match {
+        case 1 => black
+        case -1 => white
+        case default => throw new RuntimeException
+      }
     }
 
     def containsAce(cards: List[String]): Boolean = {
@@ -16,10 +56,10 @@ object PokerHands {
       })
     }
 
-    def parseHands(input: String): (String, String)  = {
+    def parseHands(input: String): (String, String) = {
       val regex = "^Black: (.*)  White: (.*)".r
       input match {
-        case regex(black, white) =>  (black, white)
+        case regex(black, white) => (black, white)
       }
     }
 
@@ -35,14 +75,14 @@ object PokerHands {
       string: String => string(0)
     }
 
-
     val (blackHand, whiteHand) = parseHands(hands)
     val blackCards = parseCards(blackHand)
     val whiteCards = parseCards(whiteHand)
 
-    if (isTie(blackCards, whiteCards))  "Tie."
-    else s"${winningPlayer(blackCards, whiteCards)} wins. - with high card: Ace"
-
+    if (isTie(blackCards, whiteCards)) "Tie."
+    else {
+      val winner = winningPlayer(blackCards, whiteCards)
+      s"${winner.name} wins. - ${winner.hand}"
+    }
   }
-
 }
--- src/test/scala/pokerhands/PokerHandsSpec.scala
+++ src/test/scala/pokerhands/PokerHandsSpec.scala
@@ -16,6 +16,9 @@ class PokerHandsSpec extends FunSpec with ShouldMatchers {
         PokerHands.winner("Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S AH") should be ("White wins. - with high card: Ace")
         PokerHands.winner("Black: 2H 3D 5S 9C AD  White: 2D 3H 5C 9S KH") should be ("Black wins. - with high card: Ace")
       }
+      it("should announce the correct winning card") {
+        PokerHands.winner("Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S QH") should be ("Black wins. - with high card: King")
+      }
     }
   }
 
